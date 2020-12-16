890b3c09efca147a9822d9505d2eefb32db1f110
ghanavinodhinichandrasekaran <ghanacvino@gmail.com>
2020-12-16 16:26:56 +0100

    implemented board logic

--- TicTacToeApp/GameViewController.swift
+++ TicTacToeApp/GameViewController.swift
@@ -10,44 +10,55 @@ import UIKit
 class GameViewController: ViewController {
     
     
-    @IBOutlet weak var player1TxtField: UITextField!
-    @IBOutlet weak var player1ScoreLabel: UILabel!
-    @IBOutlet weak var player2TxtField: UITextField!
-    @IBOutlet weak var player2ScoreLabel: UILabel!
+    @IBOutlet var boxes: [UIButton]!
+    var board = [String]()
+    var currentPlayer = ""
     
-    @IBOutlet weak var box1: UIButton!
-    @IBOutlet weak var box2: UIButton!
-    @IBOutlet weak var box3: UIButton!
-    @IBOutlet weak var box4: UIButton!
-    @IBOutlet weak var box5: UIButton!
-    @IBOutlet weak var box6: UIButton!
-    @IBOutlet weak var box7: UIButton!
-    @IBOutlet weak var box8: UIButton!
-    @IBOutlet weak var box9: UIButton!
-    
-    var currentPlayer = 1 //Cross
+   
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadBoard()
+        print(board)
     
     }
     
-    @IBAction func player2TxtField(_ sender: UITextField) {
+  
+    @IBAction func boxPressed(_ sender: UIButton) {
+        let index = boxes.index(of:sender)!
+        print(index)
+        
+       /* if currentPlayer == 1
+        {
+            sender.setImage(UIImage(named: "Cancel.png"), for: <#T##UIControl.State#>)
+            currentPlayer = 2
+        }
+        else{
+            sender.setImage(UIImage(named: "circle.png"), for: <#T##UIControl.State#>)
+            currentPlayer = 1
+        }*/
+        
+        if !board[index].isEmpty{
+            return
+        }
+        if currentPlayer == "X"{
+            sender.setTitle("X", for: .normal)
+            currentPlayer = "O"
+            board[index] = "X"
+        }else{
+            sender.setTitle("O", for: .normal)
+            currentPlayer = "X"
+            board[index] = "O"
+        }
+        
     }
     
-    @IBAction func player1TxtField(_ sender: UITextField) {
+   func loadBoard(){
+        for i in 0..<boxes.count{
+            board.append("")
+        }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
