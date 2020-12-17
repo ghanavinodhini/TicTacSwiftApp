7f29360b32150458448b0481838c0c37bc7cc9ce
ghanavinodhinichandrasekaran <ghanacvino@gmail.com>
2020-12-17 10:03:53 +0100

    implemented winrules logic

--- TicTacToeApp/GameViewController.swift
+++ TicTacToeApp/GameViewController.swift
@@ -13,6 +13,12 @@ class GameViewController: ViewController {
     @IBOutlet var boxes: [UIButton]!
     var board = [String]()
     var currentPlayer = ""
+    var playerXScore = 0
+    var playerOScore = 0
+    var winPlayer = ""
+    
+    //2D array of array of subarrays
+    var winRules = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     
    
     
@@ -20,43 +26,86 @@ class GameViewController: ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadBoard()
-        print(board)
+       // print(board)
     
     }
     
   
-    @IBAction func boxPressed(_ sender: UIButton) {
-        let index = boxes.index(of:sender)!
-        print(index)
+    @IBAction func boxPressed(_ sender: UIButton)
+    {
+        let index = boxes.firstIndex(of:sender)!
+      
         
-       /* if currentPlayer == 1
+        //to avoid allowing user to change symbol after one turn.
+        if !board[index].isEmpty
         {
-            sender.setImage(UIImage(named: "Cancel.png"), for: <#T##UIControl.State#>)
-            currentPlayer = 2
-        }
-        else{
-            sender.setImage(UIImage(named: "circle.png"), for: <#T##UIControl.State#>)
-            currentPlayer = 1
-        }*/
-        
-        if !board[index].isEmpty{
             return
         }
-        if currentPlayer == "X"{
+        if currentPlayer == "X"
+        {
             sender.setTitle("X", for: .normal)
             currentPlayer = "O"
             board[index] = "X"
-        }else{
+        }else
+        {
             sender.setTitle("O", for: .normal)
             currentPlayer = "X"
             board[index] = "O"
         }
+         
+            winners()
         
     }
     
    func loadBoard(){
-        for i in 0..<boxes.count{
+    //use '_' to use for loop without variables.
+    //Use '<' to exclude  iteration of (count value-1)
+    //add to an array using 'append'
+    for _ in 0..<boxes.count{
             board.append("")
+        }
+    }
+    
+    func winners()
+    {
+        for rule in winRules
+        {
+            //print(rule)
+        
+            let playerAt0 = board[rule[0]]
+            let playerAt1 = board[rule[1]]
+            let playerAt2 = board[rule[2]]
+            
+            if playerAt0 == playerAt1,
+               playerAt1 == playerAt2,
+               !playerAt0.isEmpty
+            {
+               print("Winner inside winners function is: \(playerAt0)")
+                winPlayer = playerAt0
+                showAlert()
+                
+            }
+        }
+       
+    }
+    
+    func showAlert(){
+        let alert = UIAlertController(title: "Success", message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default){_ in
+            self.resetBoard()
+        }
+        print(alert.message = "Player WON : " + winPlayer)
+        alert.message = "Player WON : " + winPlayer
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    func resetBoard()
+    {
+        board.removeAll()
+        loadBoard()
+        
+        for button in boxes{
+            button.setTitle(nil, for: .normal)
         }
     }
 }
